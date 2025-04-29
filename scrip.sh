a6bf34c8570e8efb9abdab5a6ec54bc8de355c58
richards28 <richirocks96@gmail.com>
2025-04-29 11:05:19 +0530

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -1,1 +1,12 @@
-
+ #!/bin/bash
+ yum update -y
+ curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+ yum install -y nodejs git
+ npm install -g yarn pm2
+ git clone https://github.com/richards28/strapi.git /home/ec2-user/strapi
+ cd /home/ec2-user/strapi
+ yarn install
+ yarn build
+ pm2 start yarn --name strapi -- start
+ pm2 save
+ pm2 startup
